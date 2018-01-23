69e68a51ca891b445a3b82af7e94eb4b0e9265ab
rocknes <aritra.iitkgp@gmail.com>
2018-01-23 13:50:02 -0800

    VCDA-395 and VCDA-396: Adding commands for listing and deleting org vdc networks
    * Added commands to list and delete both direct networks and isolated org vdc networks.
    
    Manual testing of commands:
    (vcd-cli) C:\code\vcd-cli\vcd_cli>vcd network direct list
    name
    [------]
    dnet1
    Dnet1
    
    (vcd-cli) C:\code\vcd-cli\vcd_cli>vcd network direct delete dnet1
    Are you sure you want to delete the OrgVdc Network? [y/N]: y
    networkDelete: Deleting Network dnet1(3c3a916e-6601-4df5-84f0-804d2774757f)
    task: 93732628-5b4f-45e2-ad10-9c8302b12e13, Deleted Network dnet1(3c3a916e-6601-4df5-84f0-804d2774757f), result: success
    
    To test --force option, I attached the network to a vApp and tried to delete the network with and without the option --force
    (vcd-cli) C:\code\vcd-cli\vcd_cli>vcd network direct delete Dnet1
    Are you sure you want to delete the OrgVdc Network? [y/N]: y
    Usage: vcd network direct delete [OPTIONS]
    
    Error: Status code: 400/BAD_REQUEST, [ 0d0a6cc5-d12b-4b81-a8be-183380c89113 ] Entity Dnet1 cannot be deleted, because it is in use. (request id: 0d0a6cc5-d12b-4b81-a8be-183380c89113)
    
    (vcd-cli) C:\code\vcd-cli\vcd_cli>vcd network direct delete Dnet1 -f
    Are you sure you want to delete the OrgVdc Network? [y/N]: y
    networkDelete: Deleting Network Dnet1(77936cfb-58c9-44bb-b958-732f928f13e3)
    task: e8013c54-4726-4e45-8ace-3cd18d2d35c4, Deleted Network Dnet1(77936cfb-58c9-44bb-b958-732f928f13e3), result: success
    
    (vcd-cli) C:\code\vcd-cli\vcd_cli>vcd network isolated list
    name
    [--------]
    iso-net1
    
    (vcd-cli) C:\code\vcd-cli\vcd_cli>vcd network isolated delete iso-net1
    Are you sure you want to delete the OrgVdc Network? [y/N]: y
    networkDelete: Deleting Network iso-net1(80980e57-7d23-4efb-b0d6-86aebfe10949)
    task: a99eb56c-0a2a-4d96-8e47-7966fabe7ef2, Deleted Network iso-net1(80980e57-7d23-4efb-b0d6-86aebfe10949), result: success

--- vcd_cli/network.py
+++ vcd_cli/network.py
@@ -19,6 +19,7 @@ from pyvcloud.vcd.vdc import VDC
 from vcd_cli.utils import restore_session
 from vcd_cli.utils import stderr
 from vcd_cli.utils import stdout
+from vcd_cli.vcd import abort_if_false
 from vcd_cli.vcd import vcd
 
 
@@ -41,6 +42,9 @@ def external(ctx):
     """Work with external networks.
 
 \b
+    Note
+        Only System Administrators can work with external networks.
+\b
     Examples
         vcd network external list
             List all external networks available in the system
@@ -58,12 +62,22 @@ def direct(ctx):
     """Work with directly connected org vdc networks.
 
 \b
+    Note
+        System Administrators have full control on direct org vdc networks.
+        Organization Administrators can only list direct org vdc networks.
+\b
     Examples
         vcd network direct create direct-net1 \\
-            --description 'Directly connected VDC network' \\
-            --parent ext-net1 \\
+                --description 'Directly connected VDC network' \\
+                --parent ext-net1 \\
             Create an org vdc network which is directly connected
             to an external network.
+\b
+        vcd network direct list
+            List all directly connected org vdc networks in the selected vdc
+\b
+        vcd network direct delete direct-net1
+            Delete directly connected network 'direct-net1' in the selected vdc
     """
     if ctx.invoked_subcommand is not None:
         try:
@@ -78,15 +92,25 @@ def isolated(ctx):
     """Work with isolated org vdc networks.
 
 \b
+    Note
+        Both System Administrators and Organization Administrators can create,
+        delete or list isolated org vdc networks.
+\b
     Examples
         vcd network isolated create isolated-net1 --gateway-ip 192.168.1.1 \\
-            --netmask 255.255.255.0 --description 'Isolated VDC network' \\
-            --primary-dns-ip 8.8.8.8 --dns-suffix example.com \\
-            --ip-range-start 192.168.1.100 --ip-range-end 192.168.1.199 \\
-            --dhcp-enabled --default-lease-time 3600 \\
-            --max-lease-time 7200 --dhcp-ip-range-start 192.168.1.100 \\
-            --dhcp-ip-range-end 192.168.1.199
+                --netmask 255.255.255.0 --description 'Isolated VDC network' \\
+                --primary-dns-ip 8.8.8.8 --dns-suffix example.com \\
+                --ip-range-start 192.168.1.100 --ip-range-end 192.168.1.199 \\
+                --dhcp-enabled --default-lease-time 3600 \\
+                --max-lease-time 7200 --dhcp-ip-range-start 192.168.1.100 \\
+                --dhcp-ip-range-end 192.168.1.199
             Create an isolated org vdc network with an inbuilt dhcp service.
+\b
+        vcd network isolated list
+            List all isolated org vdc networks in the selected vdc
+\b
+        vcd network isolated delete isolated-net1
+            Delete isolated network 'isoalted-net1' in the selected vdc
     """
     if ctx.invoked_subcommand is not None:
         try:
@@ -269,5 +293,110 @@ def list_external_networks(ctx):
         for ext_net in ext_nets:
             result.append({'name': ext_net.get('name')})
         stdout(result, ctx)
+    except Exception as e:
+        stderr(e, ctx)
+
+
+@direct.command(
+    'list',
+    short_help='list all directly connected org vdc networks in the selected'
+    ' vdc')
+@click.pass_context
+def list_direct_networks(ctx):
+    try:
+        client = ctx.obj['client']
+        in_use_vdc_href = ctx.obj['profiles'].get('vdc_href')
+        vdc = VDC(client, href=in_use_vdc_href)
+
+        direct_nets = vdc.list_orgvdc_direct_networks()
+
+        result = []
+        for direct_net in direct_nets:
+            result.append({'name': direct_net.get('name')})
+        stdout(result, ctx)
+    except Exception as e:
+        stderr(e, ctx)
+
+
+@isolated.command(
+    'list',
+    short_help='list all isolated org vdc networks in the selected vdc')
+@click.pass_context
+def list_isolated_networks(ctx):
+    try:
+        client = ctx.obj['client']
+        in_use_vdc_href = ctx.obj['profiles'].get('vdc_href')
+        vdc = VDC(client, href=in_use_vdc_href)
+
+        isolated_nets = vdc.list_orgvdc_isolated_networks()
+
+        result = []
+        for isolated_net in isolated_nets:
+            result.append({'name': isolated_net.get('name')})
+        stdout(result, ctx)
+    except Exception as e:
+        stderr(e, ctx)
+
+
+@direct.command(
+    'delete',
+    short_help='delete a directly connected org vdc network in the selected'
+    ' vdc')
+@click.pass_context
+@click.argument('name', metavar='<name>')
+@click.option(
+    '-f',
+    '--force',
+    is_flag=True,
+    default=False,
+    help='pass this option to force delete an org vdc network')
+@click.option(
+    '-y',
+    '--yes',
+    is_flag=True,
+    callback=abort_if_false,
+    expose_value=False,
+    prompt='Are you sure you want to delete the OrgVdc Network?')
+def delete_direct_networks(ctx, name, force):
+    try:
+        client = ctx.obj['client']
+        in_use_vdc_href = ctx.obj['profiles'].get('vdc_href')
+        vdc = VDC(client, href=in_use_vdc_href)
+
+        delete_task = vdc.delete_direct_orgvdc_network(name=name, force=force)
+
+        stdout(delete_task, ctx)
+    except Exception as e:
+        stderr(e, ctx)
+
+
+@isolated.command(
+    'delete',
+    short_help='delete an isolated org vdc network in the selected vdc')
+@click.pass_context
+@click.argument('name', metavar='<name>')
+@click.option(
+    '-f',
+    '--force',
+    is_flag=True,
+    default=False,
+    help='pass this option to force delete an org vdc network')
+@click.option(
+    '-y',
+    '--yes',
+    is_flag=True,
+    callback=abort_if_false,
+    expose_value=False,
+    prompt='Are you sure you want to delete the OrgVdc Network?')
+def delete_isolated_networks(ctx, name, force):
+    try:
+        client = ctx.obj['client']
+        in_use_vdc_href = ctx.obj['profiles'].get('vdc_href')
+        vdc = VDC(client, href=in_use_vdc_href)
+
+        delete_task = vdc.delete_isolated_orgvdc_network(
+            name=name, force=force)
+
+        stdout(delete_task, ctx)
     except Exception as e:
         stderr(e, ctx)

1af843a9d5d2edeb37367024c70b8b8099aac25c
Curt Kolovson <ckolovson@vmware.com>
2018-03-06 12:16:11 -0800

    VDP-1375, VCDA-504: Attach VC (#194)
    * added attach VC to vcd-cli

--- vcd_cli/vc.py
+++ vcd_cli/vc.py
@@ -33,6 +33,18 @@ def vc(ctx):
 \b
         vcd vc info vc1
             Get details of the vCenter Server 'vc1' attached to the vCD system.
+\b
+        vcd vc attach vc-name
+            --vc-host 'VC-host-FQDN-or-IP-address'
+            --vc-user 'vc-admin-user-name'
+            --vc-pwd 'vc-admin-user-password'
+            --nsx-server-name 'nsx-server-namespace'
+            --nsx-host 'NSX-host-FQDN-or-IP-address'
+            --nsx-user 'nsx-admin-user-name'
+            --nsx-pwd 'nsx-admin-password'
+            --enable
+                Attaches Virtual Center (VC) server with the given
+                credentials to vCD.
     """
     pass
 
@@ -56,5 +68,77 @@ def info(ctx, name):
         restore_session(ctx)
         platform = Platform(ctx.obj['client'])
         stdout(platform.get_vcenter(name), ctx)
+    except Exception as e:
+        stderr(e, ctx)
+
+
+@vc.command('attach', short_help='attach vCenter Server')
+@click.pass_context
+@click.argument('vc-name', metavar='<vc-name>', required=True)
+@click.option(
+    '--vc-host',
+    required=True,
+    default=None,
+    metavar='[vc-host]',
+    help='VC host FQDN or IP address')
+@click.option(
+    '--vc-user',
+    required=True,
+    default=None,
+    metavar='[vc-user]',
+    help='VC admin user name')
+@click.option(
+    '--vc-pwd',
+    required=True,
+    default=None,
+    metavar='[vc-pwd]',
+    help='VC admin password')
+@click.option(
+    '--nsx-server-name',
+    required=False,
+    default=None,
+    metavar='[nsx-server-name]',
+    help='NSX server name')
+@click.option(
+    '--nsx-host',
+    required=False,
+    default=None,
+    metavar='[nsx-host]',
+    help='NSX host FQDN or IP address')
+@click.option(
+    '--nsx-user',
+    required=False,
+    default=None,
+    metavar='[nsx-user]',
+    help='NSX admin user name')
+@click.option(
+    '--nsx-pwd',
+    required=False,
+    default=None,
+    metavar='[nsx-pwd]',
+    help='NSX admin password')
+@click.option(
+    '-e',
+    '--enable',
+    is_flag=True,
+    default=None,
+    metavar='[enable]',
+    help='enable flag (enables VC when it is attached to vCD)')
+def attach(ctx, vc_name, vc_host, vc_user, vc_pwd,
+           nsx_server_name, nsx_host, nsx_user, nsx_pwd, enable):
+    try:
+        restore_session(ctx)
+        client = ctx.obj['client']
+        platform = Platform(client)
+        platform.attach_vcenter(vc_server_name=vc_name,
+                                vc_server_host=vc_host,
+                                vc_admin_user=vc_user,
+                                vc_admin_pwd=vc_pwd,
+                                nsx_server_name=nsx_server_name,
+                                nsx_host=nsx_host,
+                                nsx_admin_user=nsx_user,
+                                nsx_admin_pwd=nsx_pwd,
+                                is_enabled=enable)
+        stdout('VirtualCenter server attached successfully.', ctx)
     except Exception as e:
         stderr(e, ctx)

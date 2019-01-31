f688a8484570467f262f295e9e7c6c39992e67c0
Rajesh <rajeshk@vmware.com>
2019-01-31 17:15:54 +0530

    Fixed flake8 issues

--- system_tests/run_system_tests.sh
+++ system_tests/run_system_tests.sh
@@ -30,7 +30,7 @@ cd $SRCROOT
 
 # If there are tests to run use those. Otherwise use stable tests.
 STABLE_TESTS="login_and_vcd_tests.py \
-dhcp_pool_tests.py \
+dhcp_pool_test.py \
 extnet_tests.py \
 firewall_rule_tests.py \
 gateway_tests.py \

5588fe1a702d21c63029e9b061a43ab5b112c740
kousgy123 <44152564+kousgy123@users.noreply.github.com>
2019-02-15 15:00:40 +0530

    VP-1593: Change the NAT rule sequence (#362)

--- vcd_cli/nat_rule.py
+++ vcd_cli/nat_rule.py
@@ -35,26 +35,26 @@ def nat(ctx):
             vcd gateway services nat create-snat test_gateway1
                     --original-ip 2.2.3.12 --translated-ip 2.2.3.14 --desc
                     "SNAT Created" --vnic 0 --enabled --logging-enabled
-                create new SNAT rule
+                Create new SNAT rule
 
 \b
              vcd gateway services nat update-snat test_gateway1 196609
                      --original-ip 2.2.3.12 --translated-ip 2.2.3.14 --desc
                      "SNAT Updated" --vnic 0
-                 update SNAT rule
+                 Update SNAT rule
 
 \b
             vcd gateway services nat create-dnat test_gateway1
                     --original-ip 2.2.3.12 --translated-ip 2.2.3.14 --desc
                     "DNAT Created" --vnic 0 --protocol tcp -op 80 -tp 80
                     --enabled --logging-enabled
-                create new DNAT rule
+                Create new DNAT rule
 
 \b
             vcd gateway services nat update-dnat test_gateway1 196609
                      --original-ip 2.2.3.12 --translated-ip 2.2.3.14 --desc
                      "DNAT Updated" --vnic 0 --protocol udp -op 80 -tp 80
-                update DNAT rule
+                Update DNAT rule
 
 \b
             vcd gateway services nat list test_gateway1
@@ -67,6 +67,10 @@ def nat(ctx):
 \b
            vcd gateway services nat info test_gateway1 196609
                Get details of NAT rule
+
+\b
+           vcd gateway services nat reorder test_gateway1 196609 --index 2
+               Reorder the NAT rule position on gateway
     """
 
 
@@ -431,5 +435,24 @@ def update_dnat_rule(ctx, gateway_name, rule_id, original_address,
             original_port=original_Port,
             translated_port=translated_Port)
         stdout('DNAT rule updated successfully.', ctx)
+    except Exception as e:
+        stderr(e, ctx)
+
+
+@nat.command('reorder', short_help='reorder NAT rule position on gateway')
+@click.pass_context
+@click.argument('gateway_name', metavar='<gateway name>', required=True)
+@click.argument('rule_id', metavar='<nat rule id>', required=True)
+@click.option(
+    '--index',
+    'index',
+    metavar='<int>',
+    type=int,
+    help='index where NAT rule will be moved/shifted')
+def reorder_nat_rule(ctx, gateway_name, rule_id, index):
+    try:
+        gateway_resource = get_gateway(ctx, gateway_name)
+        gateway_resource.reorder_nat_rule(rule_id, position=index)
+        stdout('NAT rule reordered successfully.', ctx)
     except Exception as e:
         stderr(e, ctx)

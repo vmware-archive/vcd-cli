b75cfa52765002ef0d0bc82d5eed37df70b80c47
Rajesh <rajeshk@vmware.com>
2019-02-15 13:40:09 +0530

    Refactored variable name

--- system_tests/main.py
+++ system_tests/main.py
@@ -6,13 +6,13 @@ import xmlrunner
 
 if __name__ == '__main__':
     test_loader = unittest.TestLoader()
-    root_dir = os.path.dirname(__file__)
+    current_dir = os.path.dirname(__file__)
     stable_tests = []
     for test in sys.argv[1:]:
         if test.find('.py'):
             test = test.replace('.py', '')
         stable_tests.append(test)
-    testRunner = xmlrunner.XMLTestRunner(output=root_dir + 'test-reports')
+    testRunner = xmlrunner.XMLTestRunner(output=current_dir + 'test-reports')
     package = test_loader.loadTestsFromNames(names=stable_tests)
 
     try:

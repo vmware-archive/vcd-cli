27e3bddc5cb7e78096cd9e9fce402b1c76b2eb1d
SAURABH PANDEY <pandeys@vmware.com>
2019-08-21 16:57:03 +0530

    VP-2705,VP-2885: List Independent disk, enable Nested hypervisor of VM. (#484)
    * VP-2705,VP-2885: List Independent disk, enable Nested hypervisor of VM.
    
    VP-2705:[VcdCLI]List Independent disk in VDC
    VP-2885:[PYSDK] enableNestedHypervisor of VM
    
    This CLN contains List endependent disk in VDC and enable nested
    hypervisor of VM. It can be called from command line as :
    vcd vm enable-nested-hypervisor vapp1 vm1
    vcd vdc list-disk
    
    Testing Done:
    Test method test_0020_list_disk is added in vdc_tests.py class. Test
    method test_0510_enable_nested_hypervisor is added in vm_tests.py class.
    
    * VP-2705,VP-2885: List Independent disk, enable Nested hypervisor of VM.
    
    VP-2705:[VcdCLI]List Independent disk in VDC
    VP-2885:[PYSDK] enableNestedHypervisor of VM
    
    This CLN contains List endependent disk in VDC and enable nested
    hypervisor of VM. It can be called from command line as :
    vcd vm enable-nested-hypervisor vapp1 vm1
    vcd vdc list-disk
    
    Testing Done:
    Test method test_0020_list_disk is added in vdc_tests.py class. Test
    method test_0510_enable_nested_hypervisor is added in vm_tests.py class.

--- system_tests/vdc_tests.py
+++ system_tests/vdc_tests.py
@@ -17,16 +17,22 @@ from click.testing import CliRunner
 from pyvcloud.system_test_framework.base_test import BaseTestCase
 from pyvcloud.system_test_framework.environment import CommonRoles
 from pyvcloud.system_test_framework.environment import Environment
+from pyvcloud.system_test_framework.utils import create_independent_disk
+from pyvcloud.vcd.vdc import VDC
 
 from vcd_cli.login import login, logout
 from vcd_cli.org import org
 from vcd_cli.vdc import vdc
+
 
 class TestOrgVDC(BaseTestCase):
     """Test OrgVDC functionalities implemented in pyvcloud."""
 
     # All tests in this module should run as System Administrator.
     _client = None
+    _idisk_name = 'vdcSCSI'
+    _idisk_size = '5242880'
+    _idisk_description = '5Mb SCSI disk'
 
     def test_0000_setup(self):
         """Setup the org vdc required for the other tests in this module.
@@ -41,6 +47,16 @@ class TestOrgVDC(BaseTestCase):
         TestOrgVDC._runner.invoke(org, ['use', default_org])
         TestOrgVDC._vdc_resource = Environment.get_test_vdc(
             TestOrgVDC._client).get_resource()
+        TestOrgVDC._vdc1 = VDC(TestOrgVDC._client,
+                               href=TestOrgVDC._vdc_resource.get('href'))
+        # Create Independent disk
+        TestOrgVDC._idisk_id = \
+            create_independent_disk(client=TestOrgVDC._client,
+                                    vdc=TestOrgVDC._vdc1,
+                                    name=self._idisk_name,
+                                    size=self._idisk_size,
+                                    description=self._idisk_description)
+        self.assertIsNotNone(TestOrgVDC._idisk_id)
 
     def test_0010_list_media(self):
         """Test the method VDC.list_media()."""
@@ -50,6 +66,14 @@ class TestOrgVDC(BaseTestCase):
             vdc, args=['list-media', vdc_name])
         self.assertEqual(0, result.exit_code)
 
+    def test_0020_list_disk(self):
+        """Test the method VDC.list_media()."""
+        vdc_name = TestOrgVDC._vdc_resource.get('name')
+        """Get list of disk."""
+        result = TestOrgVDC._runner.invoke(
+            vdc, args=['list-disk', vdc_name])
+        self.assertEqual(0, result.exit_code)
+
     def test_9998_teardown(self):
         """Test the method VDC.delete_vdc().
 
@@ -57,7 +81,11 @@ class TestOrgVDC(BaseTestCase):
 
         This test passes if the task for deleting the vdc succeeds.
         """
-        pass
+        TestOrgVDC._vdc_resource
+        vdc = VDC(TestOrgVDC._client, href=TestOrgVDC._vdc_resource.get(
+            'href'))
+        task = vdc.delete_disk(name=self._idisk_name)
+        TestOrgVDC._client.get_task_monitor().wait_for_success(task=task)
 
     def test_9999_cleanup(self):
         """Release all resources held by this object for testing purposes."""
